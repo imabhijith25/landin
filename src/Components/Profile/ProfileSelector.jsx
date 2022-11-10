f42bcf69d6b09096a9d47c9e5524fb67917de7cc
Abhi <mailtoabhijithofficial@gmail.com>
2022-11-10 21:52:57 +0530

    image upload

--- src/Components/Profile/ProfileSelector.jsx
+++ src/Components/Profile/ProfileSelector.jsx
@@ -1,8 +1,18 @@
 import { useContext } from "react";
 import { EditorContext } from "../../Screens/Editor/Editor";
+import { storage } from "../../Firebase";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { useState } from "react";
 import styles from "./profile.module.css";
+import cn from "classnames";
+import PuffLoader from "react-spinners/PuffLoader";
 const ProfileSelector = () => {
     const editorContextvalues = useContext(EditorContext);
+    const [loader, setLoader] = useState(false);
+    const [error, setError] = useState({
+        state: false,
+        msg: "",
+    });
     const socials = [
         "Twitter",
         "Dribble",
@@ -34,11 +44,108 @@ const ProfileSelector = () => {
             },
         });
     };
+
+    const fileuploadClick = () => {
+        // setError({ state: false, msg: "" });
+        const fileButton = document.getElementById("fileuploadbutton");
+        fileButton.click();
+    };
+    const fileChange = async (e) => {
+        setLoader(true);
+        const file = e.target.files[0];
+        const allowed = ["png", "jpg", "jpeg"];
+        const type = file.type.split("/")[1];
+        if (!allowed.includes(type)) {
+            e.target.value = null;
+            setError({
+                state: true,
+                msg: "Invalid file type. Only Jpeg, png supported",
+            });
+            setLoader(false);
+            return;
+        }
+        if (file?.size / 1024 / 1024 > 5) {
+            e.target.value = null;
+            setError({ state: true, msg: "File size exceeded 5MB" });
+            setLoader(false);
+            return;
+        }
+        const storageRef = ref(storage, `files/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+            },
+            (error) => {},
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    editorContextvalues.dispatch({
+                        type: "UPDATE_PROFILE_VALUES",
+                        data: {
+                            key: "profilePic",
+                            value: downloadURL,
+                        },
+                    });
+                    setLoader(false);
+                    e.target.value = null;
+                });
+            }
+        );
+    };
+
     return (
         <div className={styles.profileSelector}>
             <h4>Your Profile</h4>
             <p>Edit your profile card with latest details</p>
             <div className={styles.editContainer}>
+                <div className={styles.uploadContainer}>
+                    <div className={styles.imageCropper}>
+                        <img
+                            className={styles.img}
+                            src={
+                                editorContextvalues?.state?.preview?.profile
+                                    ?.profilePic
+                            }
+                            style={{
+                                border: "2px solid rgb(95, 94, 94)",
+                                objectFit: "cover",
+                            }}
+                            alt="profile"
+                        ></img>
+                    </div>
+                    <input
+                        type="file"
+                        id="fileuploadbutton"
+                        accept="image/*"
+                        onChange={fileChange}
+                        style={{ display: "none" }}
+                    ></input>
+                    <button
+                        className={cn("toolButton")}
+                        onClick={fileuploadClick}
+                        disabled={loader}
+                    >
+                        {loader ? (
+                            <div
+                                style={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                <PuffLoader
+                                    size={18}
+                                    color="white"
+                                ></PuffLoader>
+                            </div>
+                        ) : (
+                            "Change display image"
+                        )}
+                    </button>
+                </div>
                 <div className={styles.info}>
                     <label>Name</label>
                     <input
@@ -67,6 +174,8 @@ const ProfileSelector = () => {
                         <p>Work</p>
                     </b>
                 </div>
+                <p>Add your work details</p>
+                <br />
                 {work.map((item, index) => (
                     <div className={styles.info} key={index}>
                         <label>{item}</label>
@@ -88,7 +197,8 @@ const ProfileSelector = () => {
                         <p>Social</p>
                     </b>
                 </div>
-
+                <p>Add your social media links</p>
+                <br />
                 {socials.map((item, index) => (
                     <div className={styles.info} key={index}>
                         <label>{item}</label>
